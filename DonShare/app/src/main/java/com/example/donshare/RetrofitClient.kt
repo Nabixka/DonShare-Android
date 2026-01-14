68b45ab934cacd1809b3317e0a3db7addc2cd1c8
Nabixka <nabixka05@gmail.com>
2026-01-14 21:49:24 +0700

    Fetch Event Donation

--- DonShare/app/src/main/java/com/example/donshare/RetrofitClient.kt
+++ DonShare/app/src/main/java/com/example/donshare/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.56.1:3000/"
+    private const val BASE_URL = "YOUR_API_URL"
 
     val instance: ApiService by lazy {
         val retrofit = Retrofit.Builder()
